deb1562af32670aee04d6445a941433148f1aab1
macaddino <l.macaddino@gmail.com>
2014-04-23 01:50:59 -0500

    Adding a pseudo-codey version of network server model

---
+++ src/engines/network/globalData.h
@@ -0,0 +1,16 @@
+/*
+*
+* CMSC 23800 / 33800 - Game Construction
+*
+* Global variables pertaining to pthreads and mutexes.
+*
+*
+*/
+
+#ifndef GLOBALDATA_H_
+#define GLOBALDATA_H_
+
+pthread_mutex_t lock;
+int num_ready_pthreads;
+
+#endif /* GLOBALDATA_H_ */
---
+++ src/engines/network/structures.h
@@ -0,0 +1,24 @@
+/*
+*
+* CMSC 23800 / 33800 - Game Construction
+*
+* Data structures
+*
+*/
+
+
+#ifndef STRUCTURES_H_
+#define STRUCTURES_H_
+
+ struct workerArgs
+{
+  int socket;
+  char * clientHost;
+  int * worldEngine;
+};
+
+struct replyPackage
+{
+};
+
+#endif /* STRUCTURES_H_ */
