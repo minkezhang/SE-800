ece4216d80b4d6c656cb135d5a43b26dd1d933cd
Laura Macaddino <macaddino@macaddino.(none)>
2014-05-07 18:47:42 -0500

    Fixing network bind bug in which network interpreted bind function as std::bind instead of bind network fxn

--- src/networks/server.cc
+++ src/networks/server.cc
@@ -11,8 +11,6 @@
 #include <sys/types.h>
 #include <string.h>
 
-using namespace std;
-
 Server::Server(int port) {
   this->accept_clients((void *)&port);
 }
@@ -28,9 +26,9 @@ bool Server::send_to_client(NetPacket *packet, int client_id) {
 }
 
 void *Server::accept_clients(void *args) {
-  cout << "ABOUT TO LISTEN FOR CLIENTS" << endl;
+  std::cout << "ABOUT TO LISTEN FOR CLIENTS" << std::endl;
   int *port = (int *) args;
-  cout << "port number is " << *port << endl;
+  std::cout << "port number is " << *port << std::endl;
   int num_clients = 0;
   int clientSocket;
   pthread_t worker_thread;
@@ -45,13 +43,16 @@ void *Server::accept_clients(void *args) {
   serverAddr.sin_addr.s_addr = INADDR_ANY;
 
   this->server_socketfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  setsockopt(this->server_socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
-  bind(this->server_socketfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-  listen(this->server_socketfd, 5);
+  if (setsockopt(this->server_socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) != 0)
+    std::cout << "ERROR WITH SETSOCKOPT" << std::endl;
+  if (::bind(this->server_socketfd, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) != 0)
+    std::cout << "ERROR WITH BIND" << std::endl;
+  if (listen(this->server_socketfd, 5) != 0)
+    std::cout << "ERROR WITH LISTEN " << std::endl;
 
   while(1) {
     clientSocket = accept(this->server_socketfd, (struct sockaddr *) &clientAddr, &sinSize);
-    cout << "Found a client!" << endl;
+    std::cout << "Found a client!" << std::endl;
     num_clients++;
     setsockopt(clientSocket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
     // Retrieve client hostname
@@ -63,14 +64,14 @@ void *Server::accept_clients(void *args) {
     // Pass on arguments to worker thread
 
     if (pthread_create(&worker_thread, NULL, serve_client, &this->server_socketfd) != 0) {
-      cout << "Could not create a worker thread." << endl;
+      std::cout << "Could not create a worker thread." << std::endl;
       num_clients--;
       close(clientSocket);
       pthread_exit(NULL);
     }
 
     if (num_clients == 1) {
-      cout << "ONE PLAYER CONNECTED!" << endl;
+      std::cout << "ONE PLAYER CONNECTED!" << std::endl;
     }
   }
 
