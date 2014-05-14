457f7073210ed2da2028c9c7dd7eaf8b3869f774
Minke Zhang <minke.zhang@gmail.com>
2014-05-13 23:32:07 -0500

    more physics engine changes

--- src/engines/physics/grid.h
+++ src/engines/physics/grid.h
@@ -12,7 +12,7 @@
 
 class Grid {
 	public:
-		Grid(int id, std::vector<float> size, std::vector<float> corner);
+		Grid(int id, std::vector<float> corner, std::vector<float> size);
 		~Grid();
 
 		std::vector<Projectile *> get_projectiles();				// returns a list of all projectiles within the grid
@@ -24,11 +24,15 @@ class Grid {
 		std::map<int, Projectile *> get_clippable();
 		std::map<int, Projectile *> get_unclippable();
 
+		std::vector<float> get_size();
+		std::vector<float> get_corner();
+
 	private:
 		int id;									// globally unique grid id -- set in constructor
 
-		std::vector<float> size;						// dimensions of the box -- set in constructor
 		std::vector<float> corner;						// lowermost corner of the box -- set in constructor
+		std::vector<float> size;						// dimensions of the box -- set in constructor
+
 		std::map<int, Projectile *> clippable;					// list of clippable objects (objects whose is_clippable() function returns true)
 		std::map<int, Projectile *> unclippable;				// list of unclippable objects (do not need to check unclippable objects colliding with unclippable objects)
 };
