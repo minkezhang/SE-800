00c77ed7aa85a8d5eb38e47a125200d5ac4d70ba
Minke Zhang <minke.zhang@gmail.com>
2014-05-22 00:10:52 -0500

    added bullets

--- src/classes/munition.h
+++ src/classes/munition.h
@@ -11,28 +11,23 @@
 
 class Munition : public Projectile {
 	public:
-		Munition(std::vector<float> origin, std::vector<float> v);	// spawn in bullet with appropriate speed and direction
+		Munition(int id, float max_tolerance, std::vector<float> origin, std::vector<float> v, float aoe, int lifetime);	// spawn in bullet with appropriate speed and direction
 		~Munition();
-
-		float get_range();						// origin - projectile.r
 
 		/**
 		 * if distance to surrounding ships < aoe, apply damage as returned by get_damage()
 		 */
-		virtual void explode();						// override this
+		virtual void explode();													// override this
 
 	private:
-		int render_class;
-		std::vector<float> origin;					// object.r at which the bullet spawned -- set in constructor
-		float range;							// distance the munition can travel -- set in constructor
-		float aoe;							// area of effect
+		float aoe;														// area of effect
 
 		/**
 		 * for ships near the munition at explosion, get damage output
 		 *
 		 * called in explode()
 		 */
-		virtual int get_damage(float distance);				// override this
+		virtual float get_damage(float distance);										// override this
 };
 
 #endif
