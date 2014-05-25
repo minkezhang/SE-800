44ebcac3401b3ffa7e61ee3e14ba4d436a2ee92d
Minke Zhang <minke.zhang@gmail.com>
2014-05-25 18:02:38 -0500

    weapon_index set in ship.init

--- src/classes/ship.cc
+++ src/classes/ship.cc
@@ -5,6 +5,7 @@
 
 Ship::Ship(int id, float mass, float max_tolerance, std::vector<float> d, float size, float preset_a, float preset_p_dot, float preset_r_dot) : Projectile(id, 1, mass, max_tolerance, d, { 0, 0, 0 }, size, { 1, 0, 0 }, { 0, 1, 0 }, 0, 0, preset_a, preset_p_dot, preset_r_dot, -1) {
 	this->type = ObjType::SHIP;
+	this->weapon_index = 0;
 }
 
 int Ship::get_weapon_index() {
